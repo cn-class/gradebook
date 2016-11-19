0fc07fe4bd1a374cdd6c696b8239a73e3ee23021
Sermsook Pulnil <sermsook.pulnil@gmail.com>
2016-11-19 19:10:02 +0700

    create Section model

--- myProject/courses/admin.py
+++ myProject/courses/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Course, Section
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
     list_filter = ('name','number','year','semester','description','major')
     search_fields = ['name','number','year','semester','major']
     list_display = ('number','name','year','semester','major','description')
 
+class SectionAdmin(admin.ModelAdmin):
+    list_filter = ('number','year','semester','range_start','range_end','grade','time')
+    search_fields = ['number','year','semester','time']
+    list_display = ('number','year','semester','range_start','range_end','grade','time')
+
 admin.site.register(Course, CourseAdmin)
+admin.site.register(Section, SectionAdmin)
--- myProject/courses/models.py
+++ myProject/courses/models.py
@@ -8,3 +8,12 @@ class Course(models.Model):
     semester = models.CharField(max_length=50)
     description = models.CharField(max_length=500)
     major = models.CharField(max_length=50)
+
+class Section(models.Model):
+    number = models.CharField(max_length=50,primary_key=True)
+    year = models.CharField(max_length=50)
+    semester = models.CharField(max_length=50)
+    range_start = models.CharField(max_length=100)
+    range_end = models.CharField(max_length=100)
+    grade = models.CharField(max_length=50)
+    time = models.DateTimeField()
