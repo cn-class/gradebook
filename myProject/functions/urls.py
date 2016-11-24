d585d005199014c530e5e1ccc91daa9bf5829f8a
littledao <littlestar.daosone@gmail.com>
2016-11-24 15:02:05 +0700

    create checkIn templates and link to other page

--- myProject/functions/urls.py
+++ myProject/functions/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from django.core.urlresolvers import reverse
 
-from .views import FunctionView, AnnounceView
+from .views import FunctionView, AnnounceView, PredictView, CheckInView
 
 urlpatterns = [
     url(r'^$', FunctionView.as_view()),
     url(r'^announce', AnnounceView.as_view()),
+    url(r'^announce/predict', PredictView.as_view()),
+    url(r'^predict', PredictView.as_view()),
+    url(r'^function', FunctionView.as_view()),
+    url(r'^checkin', CheckInView.as_view()),
 ]
--- myProject/functions/views.py
+++ myProject/functions/views.py
@@ -2,7 +2,8 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+#
+# from .forms import PredictForm
 import requests
 
 # Create your views here.
@@ -13,9 +14,31 @@ class FunctionView(TemplateView):
 
         return render(request, self.template_name)
 
+
 class AnnounceView(TemplateView):
     template_name = 'announce.html'
 
     def get(self, request):
 
         return render(request, self.template_name)
+
+class PredictView(TemplateView):
+    template_name = 'predict.html'
+
+    def get(self, request):
+
+        return render(request, self.template_name)
+
+class CheckInView(TemplateView):
+    template_name = 'checkIn.html'
+
+    def get(self, request):
+
+        return render(request, self.template_name)
+
+    # def post(self, request):
+    #     form = PredictForm(data=request.POST)
+    #     if form.is_valid():
+    #         return HttpResponseRedirect(reverse('function'))
+    #     else:
+    #         return render(request, self.template_name)
