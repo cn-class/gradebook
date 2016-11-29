ab2f69af35e9fcf2c8ebd2186777ecf842620271
littlestar-dao <dao.duan@hotmail.com>
2016-11-29 13:12:09 +0700

    Edit ForeignKey in Course model and edit filter field in Course admin

--- myProject/courses/admin.py
+++ myProject/courses/admin.py
@@ -8,34 +8,34 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ('id','course_number','name','year','semester','major','description')
 
 class SectionAdmin(admin.ModelAdmin):
-    list_filter = ('id','section_number','year','semester','course_number','grade_criteria','time','instructor_id')
-    search_fields = ['section_number','year','semester','course_number','time','instructor_id']
-    list_display = ('id','section_number','year','semester','course_number','grade_criteria','time','instructor_id')
+    list_filter = ('id','section_number','year','semester','course','time','instructor')
+    search_fields = ['section_number','year','semester','course','time','instructor']
+    list_display = ('id','section_number','year','semester','course','time','instructor')
 
 class GradeCriteriaAdmin(admin.ModelAdmin):
-    list_filter = ('id','grade_criteria','range_start','range_end','grade')
-    search_fields = ['grade_criteria','grade']
-    list_display = ('id','grade_criteria','range_start','range_end','grade')
+    list_filter = ('id','section','range_start','range_end','grade')
+    search_fields = ['section','grade']
+    list_display = ('id','section','range_start','range_end','grade')
 
 class EnrollmentAdmin(admin.ModelAdmin):
-    list_filter = ('enrollment_id','student_id','section_number','grade')
-    search_fields = ['enrollment_id','student_id','section_number','grade']
-    list_display = ('enrollment_id','student_id','section_number','grade')
+    list_filter = ('enrollment_id','student','section','grade')
+    search_fields = ['enrollment_id','student','section','grade']
+    list_display = ('enrollment_id','student','section','grade')
 
 class AssessmentAdmin(admin.ModelAdmin):
-    list_filter = ('assessment_id','section_number','assessment_type','max_point','weight','date')
-    search_fields = ['assessment_id','section_number','assessment_type','date']
-    list_display = ('assessment_id','section_number','assessment_type','max_point','weight','date')
+    list_filter = ('assessment_id','section','assessment_type','max_point','weight','date')
+    search_fields = ['assessment_id','section','assessment_type','date']
+    list_display = ('assessment_id','section','assessment_type','max_point','weight','date')
 
 class ScoreAdmin(admin.ModelAdmin):
-    list_filter = ('id','enrollment_id','assessment_id','point')
-    search_fields = ['id','enrollment_id','assessment_id','point']
-    list_display = ('id','enrollment_id','assessment_id','point')
+    list_filter = ('id','enrollment','assessment','point')
+    search_fields = ['id','enrollment','assessment','point']
+    list_display = ('id','enrollment','assessment','point')
 
 class AttendanceAdmin(admin.ModelAdmin):
-    list_filter = ('id','enrollment_id','date','status')
-    search_fields = ['id','enrollment_id','date','status']
-    list_display = ('id','enrollment_id','date','status')
+    list_filter = ('id','enrollment','date','status')
+    search_fields = ['id','enrollment','date','status']
+    list_display = ('id','enrollment','date','status')
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Section, SectionAdmin)
--- myProject/courses/models.py
+++ myProject/courses/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from accounts.models import Instructor, Student
 # Create your models here.
 class Course(models.Model):
     id = models.AutoField(primary_key=True)
@@ -12,30 +12,31 @@ class Course(models.Model):
 
 class Section(models.Model):
     id = models.AutoField(primary_key=True)
-    course_number = models.CharField(max_length=50)
+    # course_number = models.CharField(max_length=50)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     section_number = models.CharField(max_length=50)
     year = models.CharField(max_length=50)
     semester = models.CharField(max_length=50)
-    grade_criteria = models.CharField(max_length=50)
     time = models.CharField(max_length=100)
-    instructor_id = models.IntegerField()
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
 
 class GradeCriteria(models.Model):
     id = models.AutoField(primary_key=True)
-    grade_criteria = models.CharField(max_length=50)
+    # grade_criteria = models.CharField(max_length=50)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
     range_start = models.IntegerField()
     range_end = models.IntegerField()
     grade = models.CharField(max_length=50)
 
 class Enrollment(models.Model):
     enrollment_id = models.AutoField(primary_key=True)
-    student_id = models.IntegerField()
-    section_number = models.CharField(max_length=50)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
     grade = models.CharField(max_length=50)
 
 class Assessment(models.Model):
     assessment_id = models.AutoField(primary_key=True)
-    section_number = models.CharField(max_length=50)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
     assessment_type = models.CharField(max_length=100)
     max_point = models.IntegerField()
     weight = models.IntegerField()
@@ -43,12 +44,12 @@ class Assessment(models.Model):
 
 class Score(models.Model):
     id = models.AutoField(primary_key=True)
-    enrollment_id = models.IntegerField()
-    assessment_id = models.IntegerField()
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    assessment = models.ForeignKey(Assessment, on_delete=models.CASCADE)
     point = models.IntegerField()
 
 class Attendance(models.Model):
     id = models.AutoField(primary_key=True)
-    enrollment_id = models.IntegerField()
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     date = models.DateField()
     status = models.CharField(max_length=50)
