2670d8521ab7e26039bae3afe84403e0388c8019
littledao <dao.duan@hotmail.com>
2017-01-31 23:24:31 +0700

    add data

---
+++ myProject/addEnroll.sql
@@ -0,0 +1,13 @@
+attach "db.sqlite3" as db1;
+update db1.courses_enrollment set  student_id = 1 where student_id=5610613365;
+update db1.courses_enrollment set  student_id = 2 where student_id=5610611111;
+update db1.courses_enrollment set  student_id = 3 where student_id=5610612222;
+update db1.courses_enrollment set  student_id = 4 where student_id=5610613333;
+update db1.courses_enrollment set  student_id = 5 where student_id=5610614444;
+update db1.courses_enrollment set  student_id = 6 where student_id=5610615555;
+update db1.courses_enrollment set  student_id = 7 where student_id=5610616666;
+update db1.courses_enrollment set  student_id = 8 where student_id=5610617777;
+update db1.courses_enrollment set  student_id = 9 where student_id=5610618888;
+update db1.courses_enrollment set  student_id = 10 where student_id=5610619999;
+
+
--- myProject/courses/admin.py
+++ myProject/courses/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Course, Section, GradeCriteria, Enrollment, Assessment, Score, Attendance
+
+from accounts.models import Student, Instructor
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
     list_filter = ('id','name','course_number','year','semester','description','major')
@@ -10,32 +12,57 @@ class CourseAdmin(admin.ModelAdmin):
 class SectionAdmin(admin.ModelAdmin):
     list_filter = ('id','section_number','year','semester','course','time','instructor')
     search_fields = ['section_number','year','semester','course','time','instructor']
-    list_display = ('id','section_number','year','semester','course','time','instructor')
+    list_display = ('id','section_number','year','semester','course_course_number','time','instructor_instructor_id')
+    def course_course_number(self, obj):
+        return obj.course.course_number
+    def instructor_instructor_id(self, obj):
+        return obj.instructor.instructor_id
 
 class GradeCriteriaAdmin(admin.ModelAdmin):
     list_filter = ('id','section','range_start','range_end','grade')
     search_fields = ['section','grade']
-    list_display = ('id','section','range_start','range_end','grade')
+    list_display = ('id','section_course','section_section_number','range_start','range_end','grade')
+    def section_section_number(self, obj):
+        return obj.section.section_number
+    def section_course(self,obj):
+        return obj.section.course.course_number
+
 
 class EnrollmentAdmin(admin.ModelAdmin):
     list_filter = ('enrollment_id','student','section','grade')
-    search_fields = ['enrollment_id','student','section','grade']
-    list_display = ('enrollment_id','student','section','grade')
+    search_fields = ['enrollment_id','student_student_id','section_section_number','grade']
+    list_display = ('enrollment_id','student_student_id','section_course','section_section_number','grade')
+    def student_student_id(self, obj):
+        return obj.student.student_id
+    def section_section_number(self, obj):
+        return obj.section.section_number
+    def section_course(self,obj):
+        return obj.section.course.course_number
 
 class AssessmentAdmin(admin.ModelAdmin):
     list_filter = ('assessment_id','section','assessment_type','max_point','weight','date')
     search_fields = ['assessment_id','section','assessment_type','date']
-    list_display = ('assessment_id','section','assessment_type','max_point','weight','date')
+    list_display = ('assessment_id','section_course','section_section_number','assessment_type','max_point','weight','date')
+    def section_section_number(self, obj):
+       return obj.section.section_number
+    def section_course(self,obj):
+       return obj.section.course.course_number
 
 class ScoreAdmin(admin.ModelAdmin):
     list_filter = ('id','enrollment','assessment','point')
     search_fields = ['id','enrollment','assessment','point']
-    list_display = ('id','enrollment','assessment','point')
+    list_display = ('id','enrollment_enrollment_id','assessment_assessment_id','point')
+    def enrollment_enrollment_id(self, obj):
+        return obj.enrollment.enrollment_id
+    def assessment_assessment_id(self,obj):
+        return obj.assessment.assessment_id
 
 class AttendanceAdmin(admin.ModelAdmin):
     list_filter = ('id','enrollment','date','status')
     search_fields = ['id','enrollment','date','status']
-    list_display = ('id','enrollment','date','status')
+    list_display = ('id','enrollment_enrollment_id','date','status')
+    def enrollment_enrollment_id(self, obj):
+        return obj.enrollment.enrollment_id
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Section, SectionAdmin)
@@ -44,3 +71,4 @@ admin.site.register(Enrollment, EnrollmentAdmin)
 admin.site.register(Assessment, AssessmentAdmin)
 admin.site.register(Score, ScoreAdmin)
 admin.site.register(Attendance, AttendanceAdmin)
+
