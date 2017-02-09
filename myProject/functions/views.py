c557b4150f86a91fcff55fe090e1811c65b1fae2
littledao <sermsook.pulnil@gmail.com>
2017-02-09 21:53:47 +0700

    11-02-60:change search form to dropdown

--- myProject/functions/views.py
+++ myProject/functions/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404,redirect,render_to_response
 from django.views.generic import TemplateView
-from courses.models import Assessment, Score, Enrollment, Attendance
+from courses.models import Assessment, Score, Enrollment, Attendance, Course
+from django.db.models import Q
 import requests
 
 
@@ -14,76 +15,80 @@ class HomeView(TemplateView):
 
         return render(request, self.template_name)
 
-"""class FunctionView(TemplateView):
-    template_name = 'function.html'
-
-    def get(self, request):
-
-        return render(request, self.template_name)"""
-
 
 class AnnounceView(TemplateView):
     template_name = 'announce.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        course_number_query = Course.objects.all()
+
+        context = {
+            "course_number": course_number_query, 
+        }
+
+        return render(request,self.template_name,context)
 
     def post(self, request):
-        course_number = request.POST.get("select_box")
-        queryset = Assessment.objects.filter(section__course__course_number=course_number).order_by('date')
-        enrollments = Enrollment.objects.filter(section__course__course_number=course_number).order_by('student__student_id')
+        select_course_number = request.POST.get("select_box")
+        course_number_query = Course.objects.filter(~Q(course_number=select_course_number))  
+        queryset = Assessment.objects.filter(section__course__course_number=select_course_number).order_by('date')
+        enrollments = Enrollment.objects.filter(section__course__course_number=select_course_number).order_by('student__student_id')
         pointset = []
         for obj in enrollments:
             pointset.append({'sid':obj.student.student_id,'scores':Score.objects.filter(enrollment=obj).order_by('assessment__date')})
-                
-        #print (pointset)
-        #print ('\n')
-
-        #for ps in pointset:
-        #    for p in ps:
-        #        print ("%s %s" % (ps.enrollment_id , ps.assessment_id))
-        #        print (p.point)
-        #        print ('\n')
-        #print (queryset)
-        #print (queryset[1].assessment_type)
-        #print ('\n')
 
         context = {
             "object_list": queryset,
             "point_list": pointset,
-            "course_number": course_number,
+            "course_number":  course_number_query,
+            "select_course_number": select_course_number,
         }
-        return render(request,self.template_name,context)
 
+        return render(request,self.template_name,context)
 
 
 class PredictView(TemplateView):
     template_name = 'predict.html'
 
     def get(self, request):
+        course_number_query = Course.objects.all()
 
-        return render(request, self.template_name)
+        context = {
+            "course_number": course_number_query, 
+        }
+
+        return render(request,self.template_name,context)
 
     def post(self, request):
-        course_number = request.POST.get("select_box") 
+        select_course_number = request.POST.get("select_box")
+        course_number_query = Course.objects.filter(~Q(course_number=select_course_number))
+
         context = {
             
-            "course_number": course_number,
+            "course_number": course_number_query,
+            "select_course_number": select_course_number,
         }
+
         return render(request,'predictPopup.html',context)
         
-
 
 class CheckInView(TemplateView):
     template_name = 'checkIn.html'
     
     def get(self, request):
-        return render(request, self.template_name)
+        course_number_query = Course.objects.all()
+
+        context = {
+            "course_number": course_number_query,
+        }
+
+        return render(request,self.template_name,context)
 
     def post(self, request):
-        course_number = request.POST.get("select_box")
-        queryset = Attendance.objects.filter(enrollment__section__course__course_number=course_number)
-        enrollments = Enrollment.objects.filter(section__course__course_number=course_number).order_by('student__student_id')
+        select_course_number = request.POST.get("select_box")
+        course_number_query = Course.objects.filter(~Q(course_number=select_course_number))
+        queryset = Attendance.objects.filter(enrollment__section__course__course_number=select_course_number)
+        enrollments = Enrollment.objects.filter(section__course__course_number=select_course_number).order_by('student__student_id')
         attendanceset = []
         for obj in enrollments:
             attendanceset.append({'sid':obj.student.student_id, 'dates':Attendance.objects.filter(enrollment=obj).order_by('date'), 'status':Attendance.objects.filter(enrollment=obj).order_by('date')})
@@ -91,8 +96,10 @@ class CheckInView(TemplateView):
         context = {
             "object_list": queryset,
             "attendance_list": attendanceset,
-            "course_number": course_number,
+            "course_number": course_number_query,
+            "select_course_number": select_course_number,
         }
+
         return render(request,self.template_name,context)
 
 
--- myProject/instructors/views.py
+++ myProject/instructors/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from courses.models import Course
+from django.db.models import Q
 import requests
+
 
 # Create your views here.
 class HomeInstructorView(TemplateView):
@@ -13,6 +15,7 @@ class HomeInstructorView(TemplateView):
 
         return render(request, self.template_name)
 
+
 class FunctionInstructorView(TemplateView):
     template_name = 'functionInstructor.html'
 
@@ -20,19 +23,52 @@ class FunctionInstructorView(TemplateView):
 
         return render(request, self.template_name)
 
+
 class AnnounceInstructorView(TemplateView):
     template_name = 'announceInstructor.html'
 
     def get(self, request):
 
-        return render(request, self.template_name)
+        context = {}
+        
+        queryset = Course.objects.all()
+        context = {
+            "object_list": queryset,
+
+        }
+    
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        
+        return render(request,self.template_name,context)
+
 
 class CheckInInstructorView(TemplateView):
     template_name = 'checkInInstructor.html'
 
     def get(self, request):
+        course_number_query = Course.objects.all()
 
-        return render(request, self.template_name)
+        context = {
+            "course_number": course_number_query, 
+        }
+
+        return render(request,self.template_name,context)
+
+    def post(self, request):
+        select_course_number = request.POST.get("select_box")
+        course_number_query = Course.objects.filter(~Q(course_number=select_course_number))
+
+        context = {
+            
+            "course_number": course_number_query,
+            "select_course_number": select_course_number,
+        }
+
+        return render(request,self.template_name,context)
+
+
 
 class ShowAttendanceView(TemplateView):
     template_name = 'showAttendance.html'
@@ -40,6 +76,7 @@ class ShowAttendanceView(TemplateView):
     def get(self, request):
 
         return render(request, self.template_name)
+
 
 class ShowGraphView(TemplateView):
     template_name = 'showGraph.html'
@@ -53,21 +90,24 @@ class EditCourseView(TemplateView):
     template_name = 'editcourse.html'
 
     def get(self, request):
+        course_number_query = Course.objects.all()
 
-        return render(request, self.template_name)
+        context = {
+            "course_number": course_number_query, 
+        }
+
+        return render(request, self.template_name,context)
 
     def post(self, request):
         context = {}
         course_number = request.POST.get("select_box")
+        print (course_number)
+        print ('\n')
+        course_number_query = Course.objects.filter(~Q(course_number=course_number))
         if course_number != None:
             queryset = Course.objects.get(course_number=course_number)
-            context = {
-                "obj": queryset,
-                "course_number": course_number,
-
-            }
-            print (queryset)
-            print ('\n')
+        
+            
         else:
             editcourse = request.POST.get("editcourse")
             if editcourse:
@@ -75,20 +115,12 @@ class EditCourseView(TemplateView):
                 queryset = Course.objects.filter(course_number=course_number).update(
                     name=request.POST.get('name'),course_number=request.POST.get('course_number'), year=request.POST.get('year'),
                     semester=request.POST.get('semester'),description=request.POST.get('description'), major=request.POST.get('major'))
-                # obj = Course(queryset)
-            print (editcourse)
-            print ('\n')
-            print (course_number)
-            print ('\n')
-            print (queryset)
-            print ('\n')
-            
-                # obj.name.value = request.POST.get('name')
-                # obj.course_number.value = request.POST.get('course_number')
-                # obj.year.value = request.POST.get('year')
-                # obj.semester.value = request.POST.get('semester')
-                # obj.description.value = request.POST.get('description')
-                # obj.major.value = request.POST.get('major')
-                # queryset.save()
+        
+        context = {
+                "obj": queryset,
+                "course_number": course_number_query,
+                "select_course_number": course_number,
+
+        }
 
         return render(request,self.template_name,context)
